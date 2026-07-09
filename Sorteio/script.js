d03f50de8676fe5e2b1da813f5752bd73de30a11
IsaacAlves <isaacsantos.7r@gmail.com>
2026-07-09 18:56:30 -0300

    v2

--- Sorteio/script.js
+++ Sorteio/script.js
@@ -1,13 +1,13 @@
 const min = document.getElementById('Min');
 const max = document.getElementById('Max');
 const submitBtn = document.getElementById('submit-btn');
+const result = document.getElementById('result');
 
 function generateRandomNumber(min, max) { return Math.floor(Math.random() * (max - min + 1)) + min; }
 
-
-submitBtn.addEventListener('click', function() {
+submitBtn.addEventListener('click', function () {
     const minVal = parseInt(min.value);
     const maxVal = parseInt(max.value);
     const randomNumber = generateRandomNumber(minVal, maxVal);
-    alert(randomNumber);
+    result.textContent = randomNumber;
 });
