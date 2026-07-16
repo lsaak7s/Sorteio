d30a4fd17cc2fbc20c0db12898542faf62e6e777
IsaacAlves <isaacsantos.7r@gmail.com>
2026-07-16 20:42:29 -0300

    resquired min e max

--- Sorteio/script.js
+++ Sorteio/script.js
@@ -3,15 +3,26 @@ const min = document.getElementById('Min');
 const max = document.getElementById('Max');
 const submitBtn = document.getElementById('submit-btn');
 const result = document.getElementById('result');
-/*Aqui esta retirando as casas decimais e fazendo com que ele pegue o primeiro valor e o ultimo */
-function generateRandomNumber(minVal, maxVal) { return Math.floor(Math.random() * (maxVal - minVal + 1)) + minVal; }
+/*aqui esta chamando a função e passando os valores para ela, e retornando o número sorteado */
 
+/*Aqui esta retirando as casas decimais e fazendo com que ele pegue o primeiro valor e o ultimo */
 submitBtn.addEventListener('click', function () {
     /*aqui esta tranformando os valores em numeros */
     const minVal = parseInt(min.value);
     const maxVal = parseInt(max.value);
-    /*aqui esta chamando a função e passando os valores para ela, e retornando o número sorteado */
     const randomNumber = generateRandomNumber(minVal, maxVal);
     /*aqui esta exibindo o número sorteado no elemento result la no html */
     result.textContent = randomNumber;
 });
+function generateRandomNumber(minVal, maxVal) {
+
+    if (minVal >= maxVal) {
+        alert("O valor mínimo não pode ser maior ou Igual do valor máximo.");
+    }
+    else { return Math.floor(Math.random() * (maxVal - minVal + 1)) + minVal; }
+};
+
+
+
+
+
